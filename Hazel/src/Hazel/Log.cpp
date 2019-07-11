3ab37937a0e1a887ce3ff8891743c93529616297
Juan Couso <Cucsijuan@gmail.com>
2019-07-10 23:11:51 -0300

    Added spdlog logger library

---
+++ Hazel/src/Hazel/Log.cpp
@@ -0,0 +1,12 @@
+#include "Log.h"
+
+
+namespace Hazel {
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+	
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] % n: %v%$");
+	}
+}
