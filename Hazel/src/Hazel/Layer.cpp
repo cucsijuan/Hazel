7a124f258a88dca1fc91fba7ecfef710cf882a4c
Juan Couso <Cucsijuan@gmail.com>
2019-07-13 21:40:17 -0300

    Added Layers - Enden layers video

---
+++ Hazel/src/Hazel/Layer.cpp
@@ -0,0 +1,14 @@
+#include "hzpch.h"
+#include "Layer.h"
+
+namespace Hazel
+{
+	Hazel::Layer::Layer(const std::string& debugName /*= "Layer"*/)
+		: m_DebugName(debugName)
+	{
+	}
+
+	Layer::~Layer()
+	{
+	}
+}
