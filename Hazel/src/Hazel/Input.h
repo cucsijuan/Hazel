305bb8e51083afc495f164c2f4f3e248c3fa9e58
Cucsijuan <cucsijuan@gmail.com>
2019-07-18 15:27:58 -0300

    Input Polling WIP

---
+++ Hazel/src/Hazel/Input.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+namespace Hazel
+{
+	class HAZEL_API Input
+	{
+	public:
+		static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) = 0;
+	private:
+		static Input* s_Instance;
+	};
+}
